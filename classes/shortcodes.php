07599707fa4b98dfc119b5108afb12fa714f64c0
Angela de Araujo <angela.araujo.rj@gmail.com>
2021-09-21 18:46:15 -0300

    let hash in shortcode [vimeo video={videoid}:{hash}]

--- classes/shortcodes.php
+++ classes/shortcodes.php
@@ -20,17 +20,22 @@ class shortcodes {
             
             global $DB;
             
-            $video = $DB->get_record('block_uploadvimeo_videos', ['videoidvimeo' => $args['video']]);
+            // Videos CCE: [vimeo video="609143971:b9da9695b4"]
+            list($videoidvimeo, $hash) = explode(":", $args['video']);
+            
+            $video = $DB->get_record('block_uploadvimeo_videos', ['videoidvimeo' => $videoidvimeo]);
             $account = $DB->get_record('block_uploadvimeo_account', ['id' => $video->accountid]);
             
-            // From 14-09-2021 it became mandatory to add the hash parameter to the embed url.
-            // linkd=https://vimeo.com/604940361/0425ef4c13
-            $search = 'https://vimeo.com/' . $video->videoidvimeo . '/';
-            $hash = str_replace($search, '', $video->linkvideo); 
+            if (!$hash) {
+                // From 14-09-2021 it became mandatory to add the hash parameter to the embed url.
+                // linkd=https://vimeo.com/604940361/0425ef4c13
+                $search = 'https://vimeo.com/' . $video->videoidvimeo . '/';
+                $hash = str_replace($search, '', $video->linkvideo);
+            }            
             
-            $uri = 'https://player.vimeo.com/video/'.$args['video'] .
-            '?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0' .
-            '&amp;h=' . $hash . '&amp;app_id=' . $account->app_id;
+            $uri = 'https://player.vimeo.com/video/'. $videoidvimeo . '?h='. $hash . 
+            '&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0' .
+            '&amp;app_id=' . $account->app_id;
             
             return '<iframe src="'. $uri. '" width="600" height="400" frameborder="0" allow="autoplay; fullscreen" allowfullscreen title=""></iframe>';
         } else {

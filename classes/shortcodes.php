efa613c3779cd2ef4d29283b886f06d2f19752f8
Angela de Araujo <angela.araujo.rj@gmail.com>
2021-09-22 19:16:20 -0300

    fix warnings

--- classes/shortcodes.php
+++ classes/shortcodes.php
@@ -20,22 +20,34 @@ class shortcodes {
             
             global $DB;
             
-            // Videos CCE: [vimeo video="609143971:b9da9695b4"]
-            list($videoidvimeo, $hash) = explode(":", $args['video']);
+            $app_id = '';
+            $hash = '';
             
-            $video = $DB->get_record('block_uploadvimeo_videos', ['videoidvimeo' => $videoidvimeo]);
-            $account = $DB->get_record('block_uploadvimeo_account', ['id' => $video->accountid]);
+            // Videos CCE: [vimeo video="609143971:b9da9695b4"].
+            if (strpos($args['video'], ":")) {
+                list($videoidvimeo, $hash) = explode(":", $args['video']);
+                
+            } else {
+                $videoidvimeo = $args['video'];                
+            }
             
             if (!$hash) {
-                // From 14-09-2021 it became mandatory to add the hash parameter to the embed url.
-                // linkd=https://vimeo.com/604940361/0425ef4c13
-                $search = 'https://vimeo.com/' . $video->videoidvimeo . '/';
-                $hash = str_replace($search, '', $video->linkvideo);
+                $video = $DB->get_record('block_uploadvimeo_videos', ['videoidvimeo' => $videoidvimeo]);
+                
+                if ($video) {
+                    $account = $DB->get_record('block_uploadvimeo_account', ['id' => $video->accountid]);
+                    $app_id = $account->app_id;
+                    
+                    // From 14-09-2021 it became mandatory to add the hash parameter to the embed url.
+                    // linkd=https://vimeo.com/604940361/0425ef4c13.
+                    $search = 'https://vimeo.com/' . $video->videoidvimeo . '/';
+                    $hash = str_replace($search, '', $video->linkvideo);
+                }
             }            
             
             $uri = 'https://player.vimeo.com/video/'. $videoidvimeo . '?h='. $hash . 
             '&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0' .
-            '&amp;app_id=' . $account->app_id;
+            '&amp;app_id=' . $app_id;
             
             return '<iframe src="'. $uri. '" width="600" height="400" frameborder="0" allow="autoplay; fullscreen" allowfullscreen title=""></iframe>';
         } else {
